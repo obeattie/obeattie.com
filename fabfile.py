48a16a1bfc6ad414157f9864352ae1af5e86eb6b
Oliver Beattie <oliver@obeattie.com>
2009-09-08 14:07:10 +0100

    Fabfile fail

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,10 @@
 """Fabric deployment file for obeattie.com"""
 
+# The server uses ssh keys, so no passwords here.
+config.fab_hosts = ['obeattie.com', ]
+config.fab_user = 'fabric'
+config.fab_port = 30000
+
 def deploy():
-    """Deploys the newest version of the site (this just consists of running `git pull`
-       on the server, really)."""
-    # The server uses ssh keys, so no passwords here.
-    config.fab_hosts = ['obeattie.com', ]
-    config.fab_user = 'fabric'
-    config.fab_port = 30000
-    
+    """Deploys the newest version of the site (this just consists of running `git pull`)"""
     run('cd /home/public_html/obeattie; git pull;')
